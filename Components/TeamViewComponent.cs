039f51e1c28f66067017ae783f7a36d6e8908490
nstaplet <nstaplet7@gmail.com>
2021-04-05 21:42:54 -0600

    Add project files.

---
+++ Components/TeamViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Assignment10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Components
+{
+    public class TeamViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+
+        public TeamViewComponent(BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.selectedcat = RouteData?.Values["TeamName"];
+
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Assignment10.Models;
+using Assignment10.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BowlingLeagueContext context { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext ctx)
+        {
+            _logger = logger;
+            context = ctx;
+        }
+
+        public IActionResult Index(long? teamId, string TeamName, int pageNum = 0)
+        {
+            //This is for pagination
+            int pageSize = 5;
+
+            return View(new IndexViewModel
+            {
+                Bowlers = (context.Bowlers
+                    .Where(m => m.TeamId == teamId || teamId == null)
+                    .OrderBy(m => m.BowlerFirstName)
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToList()),
+
+                PageNumberingInfo = new PageNumberingInfo
+                {
+                    NumItemsPerPage = pageSize,
+                    CurrentPage = pageNum,
+
+                    //check to see how many people have been selected
+                    TotalNumItems = (teamId == null ? context.Bowlers.Count() :
+                        context.Bowlers.Where(x => x.TeamId == teamId).Count())
+                },
+
+                TeamName = TeamName
+            });
+                
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
